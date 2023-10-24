8c60999c8d1ab130a392cc4d2650a3e9fe864216
Devil001600 <debanshu.pani8@gmail.com>
2023-10-24 22:33:58 +0530

    Commit - 001
    > added StudentApplication

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # studentDetails
 a dummy spring-boot application that allows manipulating a database with studentDetails through REST end-points
+
+## Contributors
+* [Debanshu P](https://github.com/Devil001600)
---
+++ src/main/java/com/devil16/demo/student/StudentApplication.java
@@ -0,0 +1,16 @@
+package com.devil16.demo.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+	/**
+	 * main() - 
+	 * 
+	 * @param args String[]
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+}
